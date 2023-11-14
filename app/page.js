0dccb6f232bd4e229cabd8cc1e426b88f77f779a
Centuari360 <supermansarvesh@gmail.com>
2023-11-14 11:36:52 +0500

    new file:   .eslintrc.json 	new file:   .gitignore 	new file:   README.md 	new file:   app/globals.css 	new file:   app/layout.js 	new file:   app/page.js 	new file:   containers/About/About.css 	new file:   containers/About/About.jsx 	new file:   containers/Body/Body.css 	new file:   containers/Body/Body.jsx 	new file:   containers/Contact/Contact.css 	new file:   containers/Contact/Contact.jsx 	new file:   containers/Footer/Footer.css 	new file:   containers/Footer/Footer.jsx 	new file:   containers/Header/Header.css 	new file:   containers/Header/Header.jsx 	new file:   containers/Reviews/Reviews.css 	new file:   containers/Reviews/Reviews.jsx 	new file:   info/Plan.txt 	new file:   info/generals.txt 	new file:   jsconfig.json 	new file:   next.config.js 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   tailwind.config.js
     Changes not staged for commit:
    	modified:   app/page.js

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import {Header, Body, About, Footer } from '@/containers'
+export default function Home() {
+  return (
+    <div>
+      <Header/>
+      <Body/>
+      <About/>
+      <Footer/>
+
+    </div>
+  )
+}
